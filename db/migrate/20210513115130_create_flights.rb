724736317a65e6b2cc2344d4bd30d8c79a73e64e
Ray <sewellray11+git@googlemail.com>
2021-05-13 13:09:27 +0100

    added better_errors + bulma gems, generated airports, flights, passengers and bookings models

---
+++ db/migrate/20210513115130_create_flights.rb
@@ -0,0 +1,10 @@
+class CreateFlights < ActiveRecord::Migration[6.1]
+  def change
+    create_table :flights do |t|
+      t.integer :origin_id
+      t.integer :destination_id
+
+      t.timestamps
+    end
+  end
+end
